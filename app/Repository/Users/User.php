97f7b33a1f999e843972f5496275f94702c0298e
Pcbtroniks <desarrollo.software@pcbtroniks.com>
2023-05-08 12:24:03 -0600

    Add\ UsersTable

---
+++ app/Repository/Users/User.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Repository\Users;
+
+use App\Models\User as UserModel;
+
+class User {
+
+    public static function get($limit)
+    {
+        return UserModel::paginate($limit);
+    }
+
+}
