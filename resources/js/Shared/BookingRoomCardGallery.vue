038b51f1cf85330cfdeda40e145661a9fe2e2780
Pcbtroniks <desarrollo.software@pcbtroniks.com>
2023-08-29 16:29:22 -0600

    Add\ Opacity when hover change image arrows in booking card gallery

--- resources/js/Shared/BookingRoomCardGallery.vue
+++ resources/js/Shared/BookingRoomCardGallery.vue
@@ -50,14 +50,14 @@ const handleMouseLeave = () => {
 </script>
 
 <template>
-    <div  class='h-[14.75rem] md:h-[21.25rem] md:w-2/5 bg-cover bg-center overflow-hidden flex relative'>
+    <div  class='group h-[14.75rem] md:h-[21.25rem] md:w-2/5 bg-cover bg-center overflow-hidden flex relative'>
             
-        <span @click="nextImage" class="absolute z-50 top-1/2 -translate-y-1/2 right-0 hover:bg-white/20 cursor-pointer transition-all duration-300">
+        <span @click="nextImage" class="opacity-60 group-hover:opacity-100 absolute z-50 top-1/2 -translate-y-1/2 right-0 hover:bg-white/20 cursor-pointer transition-all duration-300">
             <svg xmlns="http://www.w3.org/2000/svg" width="70px" height="70px" viewBox="0 0 24 24" fill="none">
                 <path d="M10 7L15 12L10 17" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
             </svg>
         </span>
-        <span @click="prevImage" class="absolute z-50 top-1/2 -translate-y-1/2 left-0 hover:bg-white/20 cursor-pointer transition-all duration-300">
+        <span @click="prevImage" class="opacity-60 group-hover:opacity-100 absolute z-50 top-1/2 -translate-y-1/2 left-0 hover:bg-white/20 cursor-pointer transition-all duration-300">
             <svg xmlns="http://www.w3.org/2000/svg" width="70px" height="70px" viewBox="0 0 24 24" fill="none">
                 <path d="M15 7L10 12L15 17" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
             </svg>
