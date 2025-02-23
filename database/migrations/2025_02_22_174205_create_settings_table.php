86c9f18cea9204022d7ad84d497656ae53c32eff
Jean <jeanmacario048@gmail.com>
2025-02-22 18:19:17 -0600

    WO\ Portable hotel settings

---
+++ database/migrations/2025_02_22_174205_create_settings_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name')->unique()->default('Hotel');
+            $table->text('description')->nullable()->default('');
+            $table->string('phone')->nullable();
+            $table->string('country')->nullable();
+            $table->string('state')->nullable();
+            $table->string('city')->nullable();
+            $table->string('zip_code')->nullable();
+            $table->string('logo')->nullable();
+            $table->string('address')->nullable();
+            $table->string('tax_address')->nullable();
+            $table->string('hotel_code')->unique()->default('hotel');
+            $table->text('embedded_map')->nullable()->default('');
+            $table->string('checkin_time')->nullable()->default('15:00');
+            $table->string('checkout_time')->nullable()->default('12:00');
+
+            $table->string('redirect_url')->default('/');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('settings');
+    }
+};
